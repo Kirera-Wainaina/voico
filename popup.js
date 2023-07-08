000f07cccf0a753c647f172d2a6f888ad0de10b6
Kirera Wainaina <richardwainainak@gmail.com>
2023-07-08 10:15:19 +0300

    delete previous recording function

--- popup.js
+++ popup.js
@@ -8,102 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const input = document.querySelector("input");
-input === null || input === void 0 ? void 0 : input.addEventListener("click", () => handleRecording(input));
-const audioContext = new AudioContext();
-function toggleHintAndAnimation() {
-    const hint = document.querySelector("p");
-    hint === null || hint === void 0 ? void 0 : hint.classList.toggle("hide");
-    const recordingAnimation = document.getElementById("recording-animation");
-    recordingAnimation === null || recordingAnimation === void 0 ? void 0 : recordingAnimation.classList.toggle("hide");
-}
-function handleRecording(element, existingMediaRecorder) {
-    return __awaiter(this, void 0, void 0, function* () {
-        toggleHintAndAnimation();
-        // remove prior event listeners
-        element === null || element === void 0 ? void 0 : element.replaceWith(element.cloneNode());
-        if (!existingMediaRecorder) {
-            // start recording
-            removeAudioElement();
-            triggerRecordingThroughOffscreenDocument();
-            // await startRecording()  
-        }
-        else {
-            if (existingMediaRecorder.state == "inactive") {
-                // restart recording
-                existingMediaRecorder.start();
-                removeAudioElement();
-            }
-            else {
-                // stop recording
-                existingMediaRecorder.stop();
-            }
-            const input = document.querySelector("input");
-            input === null || input === void 0 ? void 0 : input.addEventListener("click", () => handleRecording(input, existingMediaRecorder));
-        }
-    });
-}
-function saveRecordedMedia(audioData) {
-    const blob = new Blob(audioData, { type: "audio/mp3; codecs=opus" });
-    audioData = [];
-    const audioUrl = window.URL.createObjectURL(blob);
-    const audioElement = createAudioElement(audioUrl);
-    const script = document.querySelector("script");
-    script === null || script === void 0 ? void 0 : script.insertAdjacentElement("beforebegin", audioElement);
-}
-function combineAudioData(event, audioDataArray) {
-    audioDataArray.push(event === null || event === void 0 ? void 0 : event.data);
-}
-function createAudioElement(src) {
-    const audioElement = new Audio("src");
-    audioElement.setAttribute("controls", "");
-    audioElement.setAttribute("src", src);
-    return audioElement;
-}
-function removeAudioElement() {
-    const audioElement = document.querySelector("audio");
-    if (audioElement) {
-        audioElement.remove();
-    }
-}
-function startRecording_() {
-    chrome.tabCapture.capture({ audio: true }, (stream) => {
-        if (stream) {
-            // Continue to play the captured audio to the user.
-            const output = new AudioContext();
-            const source = output.createMediaStreamSource(stream);
-            source.connect(output.destination);
-            const audioData = [];
-            const mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
-            const input = document.querySelector("input");
-            input === null || input === void 0 ? void 0 : input.addEventListener("click", () => handleRecording(input, mediaRecorder));
-            mediaRecorder.addEventListener("stop", () => saveRecordedMedia(audioData));
-            mediaRecorder.addEventListener("dataavailable", event => combineAudioData(event, audioData));
-        }
-    });
-}
-function startRecording() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const stream = yield navigator.mediaDevices.getUserMedia({ audio: true });
-        const audioData = [];
-        const mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start();
-        const input = document.querySelector("input");
-        input === null || input === void 0 ? void 0 : input.addEventListener("click", () => handleRecording(input, mediaRecorder));
-        mediaRecorder.addEventListener("stop", () => saveRecordedMedia(audioData));
-        mediaRecorder.addEventListener("dataavailable", event => combineAudioData(event, audioData));
-    });
-}
+input === null || input === void 0 ? void 0 : input.addEventListener("click", triggerRecordingThroughOffscreenDocument);
+// getUserMedia needs to work through the offscreen html file
+// create offscreen document to get permission to operate the api
+chrome.offscreen.createDocument({
+    url: "offscreen-recording.html",
+    reasons: [chrome.offscreen.Reason.USER_MEDIA],
+    justification: "Record audio for transcription"
+});
 function triggerRecordingThroughOffscreenDocument() {
     return __awaiter(this, void 0, void 0, function* () {
-        // getUserMedia needs to work through the offscreen html file
-        // create offscreen document to get permission to operate the api
-        yield chrome.offscreen.createDocument({
-            url: "offscreen-recording.html",
-            reasons: [chrome.offscreen.Reason.USER_MEDIA],
-            justification: "Record audio for transcription"
-        });
         // send message to offscreen to start recording
-        yield chrome.runtime.sendMessage("start-recording");
+        yield chrome.runtime.sendMessage("handle-recording");
     });
 }
--- popup.ts
+++ popup.ts
@@ -1,112 +1,15 @@
 const input = document.querySelector("input");
-input?.addEventListener("click", () => handleRecording(input));
+input?.addEventListener("click", triggerRecordingThroughOffscreenDocument)
 
-const audioContext = new AudioContext();
-
-function toggleHintAndAnimation() : void {
-  const hint = document.querySelector("p");
-  hint?.classList.toggle("hide");
-
-  const recordingAnimation = document.getElementById("recording-animation");
-  recordingAnimation?.classList.toggle("hide");
-}
-
-async function handleRecording(element:HTMLInputElement, existingMediaRecorder?: MediaRecorder) {
-  toggleHintAndAnimation();
-
-  // remove prior event listeners
-  element?.replaceWith(element.cloneNode());
-
-  if (!existingMediaRecorder) {
-    // start recording
-    removeAudioElement();
-    triggerRecordingThroughOffscreenDocument();
-    // await startRecording()  
-  } else {
-    if (existingMediaRecorder.state == "inactive") {
-      // restart recording
-      existingMediaRecorder.start();
-      removeAudioElement();
-    } else {
-      // stop recording
-      existingMediaRecorder.stop();
-    }
-    const input = document.querySelector("input");
-    input?.addEventListener("click", () => handleRecording(input, existingMediaRecorder))  
-  }
-}
-
-function saveRecordedMedia(audioData: Array<Blob>) {
-  const blob = new Blob(audioData, { type: "audio/mp3; codecs=opus"});
-
-  audioData = [];
-  const audioUrl = window.URL.createObjectURL(blob);
-  const audioElement = createAudioElement(audioUrl);
-  const script = document.querySelector("script");
-  script?.insertAdjacentElement("beforebegin", audioElement);
-}
-
-function combineAudioData(event:BlobEvent, audioDataArray:Array<Blob>) {
-  audioDataArray.push(event?.data)
-}
-
-function createAudioElement(src: string) {
-  const audioElement = new Audio("src");
-  audioElement.setAttribute("controls", "");
-  audioElement.setAttribute("src", src);
-  return audioElement
-}
-
-function removeAudioElement() : void {
-  const audioElement = document.querySelector("audio");
-  if (audioElement) {
-    audioElement.remove();
-  }
-}
-
-function startRecording_() {
-  chrome.tabCapture.capture({ audio: true }, (stream) => {
-    if (stream) {
-      // Continue to play the captured audio to the user.
-      const output = new AudioContext();
-      const source = output.createMediaStreamSource(stream);
-      source.connect(output.destination);
-      
-      const audioData: Array<Blob> = [];
-      const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
-
-      const input = document.querySelector("input");
-      input?.addEventListener("click", () => handleRecording(input, mediaRecorder));
-
-      mediaRecorder.addEventListener("stop", () => saveRecordedMedia(audioData));
-      mediaRecorder.addEventListener("dataavailable", event => combineAudioData(event, audioData));
-    }
-  })
-}
-
-async function startRecording() {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true})
-  const audioData: Array<Blob> = [];
-  const mediaRecorder = new MediaRecorder(stream);
-  mediaRecorder.start();
-
-  const input = document.querySelector("input");
-  input?.addEventListener("click", () => handleRecording(input, mediaRecorder));
-
-  mediaRecorder.addEventListener("stop", () => saveRecordedMedia(audioData));
-  mediaRecorder.addEventListener("dataavailable", event => combineAudioData(event, audioData));
-}
+// getUserMedia needs to work through the offscreen html file
+// create offscreen document to get permission to operate the api
+chrome.offscreen.createDocument({
+  url: "offscreen-recording.html",
+  reasons: [chrome.offscreen.Reason.USER_MEDIA],
+  justification: "Record audio for transcription"
+});
 
 async function triggerRecordingThroughOffscreenDocument() {
-  // getUserMedia needs to work through the offscreen html file
-  // create offscreen document to get permission to operate the api
-  await chrome.offscreen.createDocument({
-    url: "offscreen-recording.html",
-    reasons: [chrome.offscreen.Reason.USER_MEDIA],
-    justification: "Record audio for transcription"
-  });
-
   // send message to offscreen to start recording
-  await chrome.runtime.sendMessage("start-recording");
+  await chrome.runtime.sendMessage("handle-recording");
 }
