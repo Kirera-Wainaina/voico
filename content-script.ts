e369238d2c0fc4aea33cd8cf98ebe09d225e2daa
Kirera Wainaina <richardwainainak@gmail.com>
2023-07-11 20:25:51 +0300

    write strategy to test on the content script file

---
+++ content-script.ts
@@ -0,0 +1,12 @@
+// so far I think I can get content scripts to work
+/*
+consider using the content script as an endpoint
+all click events are relayed to it
+Once it receives a click event,
+  It'll check through storage api if user_media is set up
+  if not: ask user for permission
+  if yes: decided if to start or stop recording
+
+  see if I can pass audio data through an event to popup file so an
+  audio element is created
+*/
