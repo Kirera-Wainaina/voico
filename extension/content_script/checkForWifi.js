cee3fd7b1f9011bce7fee125bfc9af884716b00a
Kirera Wainaina <richardwainainak@gmail.com>
2023-12-22 10:21:34 +0300

    Delete the module files import is not allowed

--- extension/content_script/checkForWifi.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-    // let the popup know if there is wifi
-    if (navigator.onLine) {
-        chrome.runtime.sendMessage({ name: "is_online", content: true });
-    }
-    else {
-        chrome.runtime.sendMessage({ name: "is_online", content: false });
-    }
-}
