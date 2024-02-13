987f463bb91305e6d95060438a262d4af77863db
Kirera Wainaina <richardwainainak@gmail.com>
2024-02-13 15:36:08 +0000

    Begin using FileUpload package to check if its multipart content

--- java-server/backend/src/main/java/online/voico/app/api/Transcribe.java
+++ java-server/backend/src/main/java/online/voico/app/api/Transcribe.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 
 public class Transcribe implements ApiInterface {
@@ -18,8 +21,11 @@ public class Transcribe implements ApiInterface {
 
   public void run() throws IOException {
     InputStream requestBody = exchange.getRequestBody();
+    HttpContext context = exchange.getHttpContext();
+    boolean isMultipart = ServletFileUpload.isMultipartContent();
     byte[] bytes = requestBody.readAllBytes();
     String bodyString = Arrays.toString(bytes); 
+    
 
     System.out.println(bodyString);;
   }
